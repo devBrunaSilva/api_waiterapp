7f84d1a768efde5d0ae1e1d841bc1b3a88b033ad
Bruna Silva <falecomsilvabruna@gmail.com>
2026-01-09 12:57:01 -0300

    feat: add mongoose as a dependency and update index.ts to connect to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    const app = express(); 
+    const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
